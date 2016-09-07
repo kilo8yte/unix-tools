06c892f3696cda2c82a8f5ba4186318d912c3b40
subnetz <benni@b45h.de>
2016-09-07 13:11:38 +0200

    Update mkdir.c

--- c/mkdir.c
+++ c/mkdir.c
@@ -1,11 +1,4 @@
-/*
- * ----------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 42):
- * <benni@include-benni.de> wrote this file. As long as you retain this notice you
- * can do whatever you want with this stuff. If we meet some day, and you think
- * this stuff is worth it, you can buy me a beer in return Benjamin Hartmann
- * ----------------------------------------------------------------------------
- */
+
 
 #include <unistd.h>
 #include <sys/stat.h>
